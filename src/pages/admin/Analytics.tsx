865910e95b1c33601f475ed2d26e196df6ca0173
ssahilhussain7214-ux <ssahilhussain7214@gmail.com>
2025-09-01 02:12:52 +0530

    Updated Analytics.tsx

--- src/pages/admin/Analytics.tsx
+++ src/pages/admin/Analytics.tsx
@@ -176,7 +176,3 @@ const Analytics: React.FC = () => {
             ))}
           </div>
         </div>
-
-        {/* Category Performance */}
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-xl font-bold
